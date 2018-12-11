f05b8176fce2e9d631d974c22246e7436e448801
generalcodingprojects <45795342+generalcodingprojects@users.noreply.github.com>
2018-12-11 17:24:23 +0100

    Create api.sh

---
+++ api.sh
@@ -0,0 +1,33 @@
+request() {
+
+path="/$api_version/$1/$2"
+data="$3"
+
+if [ "$1" == "public" ]; then
+  response="$(curl -s -X POST -d "$data" "https://api.kraken.com$path")"
+else
+  nonce="$(TZ=UTC date +%s%2N)"
+  api_sign="$(
+  python3 -c \
+  "
+import urllib.parse
+import time
+import hashlib
+import hmac
+import base64
+data = {}
+if '$data': data = dict(u.split('=') for u in '$data'.split('&'))
+data['nonce'] = '$nonce'
+postdata = urllib.parse.urlencode(data)
+urlpath = '$path'
+encoded = (str(data['nonce']) + postdata).encode()
+message = urlpath.encode() + hashlib.sha256(encoded).digest()
+signature = hmac.new(base64.b64decode('$api_secret'), message, hashlib.sha512)
+sigdigest = base64.b64encode(signature.digest())
+print(sigdigest.decode())
+  "
+  )"
+  response="$(curl -s -X POST -d "$data&nonce=$nonce" "https://api.kraken.com$path" -H "API-Key:$api_key" -H "API-Sign:$api_sign")"
+fi
+
+}
